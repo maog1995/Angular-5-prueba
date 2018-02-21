e13c5c1fa5748e963f56301f9d4711120d9b17ee
Miguel Angel Ortiz <ortiz.garcia.miguel.angel@gmail.com>
2018-02-21 18:03:50 +0100

    Añadidos datos de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'Crud de angular';
+
+    public people = [
+        {'name': 'Nombre1', 'phoneNumber': 999999999},
+        {'name': 'Nombre2', 'phoneNumber': 999999998},
+        {'name': 'Nombre3', 'phoneNumber': 999999997}
+    ];
 }
